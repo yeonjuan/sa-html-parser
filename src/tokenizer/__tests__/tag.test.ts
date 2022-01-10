bb228c055307e1b3eba36d1839be1a792622e49a
yeonjuan <yeonjuan93@naver.com>
2022-01-10 22:56:49 +0900

    impl

--- src/tokenizer/__tests__/tag.test.ts
+++ src/tokenizer/__tests__/tag.test.ts
@@ -6,5 +6,4 @@ const { StartTag, EndTag } = HtmlTokenType;
 describe("tokenizer - tag", () => {
   t("start only", ["<div>", [StartTag]]);
   t("basic", ["<div></div>", [StartTag, EndTag]]);
-  t("self close", ["<div />", [StartTag]]);
 });
